97c94f93e5fa5c0108be5293dce259dfceb5f12e
bekmirza-pro <bekmirzaprogrammer@yandex.ru>
2023-09-13 21:24:45 +0500

    images one

--- src/app.ts
+++ src/app.ts
@@ -134,10 +134,11 @@ bot.on('callback_query', async (query: any) => {
 
     const foodData = await Food.findById({ _id: _id })
 
+    
     // console.log(`http://localhost:3000/api/api/file/${foodData[0].images[0]}`);
 
     if (foodData) {
-        bot.sendPhoto(chatId, `https://pro.zirapcha.uz/api/api/file/${foodData[0].images[0]}`, {
+        bot.sendPhoto(chatId, `https://pro.zirapcha.uz/api/api/file/${foodData?.images[0]}`, {
             caption: `Nomi: ${foodData.name}\nTa'rif: ${foodData.description}\nNarxi: ${foodData.price}`,
             reply_markup: {
                 keyboard: [
